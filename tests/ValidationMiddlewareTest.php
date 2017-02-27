c1672149a96e25fd4a2bb06aa0c726bf40163e0c
Runnerlee <runnerleer@gmail.com>
2017-02-27 10:58:32 +0800

    init

---
+++ tests/ValidationMiddlewareTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @time: 17-2-27 10:58
+ */
+
+use Runner\Validation\ValidationMiddleware;
+
+class ValidationMiddlewareTest extends PHPUnit_Framework_TestCase
+{
+
+}
